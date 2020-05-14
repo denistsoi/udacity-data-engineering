6050b84f7d7e80f841568754dad2ba315b556c07
Denis Tsoi <denistsoi@gmail.com>
2020-05-14 10:30:35 +0800

    ADD: etl.py;

--- 01_data_modelling_project/etl.py
+++ 01_data_modelling_project/etl.py
@@ -7,37 +7,40 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[["song_id", "title", "artist_id",
+                    "year", "duration"]].values[0].tolist()
     cur.execute(song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = 
+    artist_data = df[["artist_id", "artist_name", "artist_location",
+                      "artist_latitude", "artist_longitude"]].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page == "NextSong"]
 
     # convert timestamp column to datetime
-    t = 
-    
+    t = pd.to_datetime(df["ts"], unit="ms")
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = [t, t.dt.hour, t.dt.day, t.dt.week,
+                 t.dt.month, t.dt.year, t.dt.weekday]
+    column_labels = ['ts', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -45,13 +48,27 @@ def process_log_file(cur, filepath):
 
     # insert songplay records
     for index, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (
+            pd.to_datetime(row.ts, unit='ms'),
+            row.userId,
+            row.level,
+            songid,
+            artistid,
+            row.sessionId,
+            row.location,
+            row.userAgent
+        )
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -59,8 +76,8 @@ def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
@@ -75,11 +92,14 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
+    print("start done.")
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    print("job done.")
 
     conn.close()
 
